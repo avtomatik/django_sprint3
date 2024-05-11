3588153d7f8bd8eb2380fd084afd5212920c39a8
Alexander <kkruscherr@gmail.com>
2024-05-11 14:04:11 +0300

    Implemented Admin Registration with Decorators

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -3,18 +3,16 @@ from django.contrib import admin
 from .models import Category, Location, Post
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    pass
 
 
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    ...
+    pass
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    ...
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Location, LocationAdmin)
-admin.site.register(Post, PostAdmin)
+    pass
